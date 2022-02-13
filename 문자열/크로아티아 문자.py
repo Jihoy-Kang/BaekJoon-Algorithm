a63a1213a7f9e747311177e38b2a6d2c06d4b8f8
Jihyo Kang <luxuryjihyo@naver.com>
2022-02-13 11:40:59 +0900

    13 Feb 2022

--- 문자열/크로아티아 문자.py
+++ 문자열/크로아티아 문자.py
@@ -1,3 +1,16 @@
+#내풀이
+croatia = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+
+a = input()
+
+for i in croatia :
+    a = a.replace(i,'*')
+
+print(len(a))
+
+
+
+#모범답안
 word = ["c=", "c-", "dz=", "d-","lj", "nj", "s=", "z="]
 
 input_data = input()
@@ -7,3 +20,5 @@ for i in word:
     input_data = input_data.replace(i, '*')
 
 print(len(input_data))
+
+
